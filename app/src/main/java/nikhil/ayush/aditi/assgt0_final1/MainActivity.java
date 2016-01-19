0a492d578de5f23cabff6b49bf18eed98e80d735
Nikhil Gupta <Nikhil@Nikhils-MacBook-Air.local>
2016-01-19 18:18:41 +0530

    Modified UI and Animations

--- app/src/main/java/nikhil/ayush/aditi/assgt0_final1/MainActivity.java
+++ app/src/main/java/nikhil/ayush/aditi/assgt0_final1/MainActivity.java
@@ -11,8 +11,11 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -21,7 +24,9 @@ import android.content.Intent;
 import android.view.MotionEvent;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
-
+import android.util.DisplayMetrics;
+import android.view.animation.TranslateAnimation;
+import android.graphics.drawable.TransitionDrawable;
 
 
 import com.android.volley.Request;
@@ -54,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.content_main);
+        setContentView(R.layout.activity_main);
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
         //      Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -111,8 +116,77 @@ public class MainActivity extends AppCompatActivity {
 //        add.setChecked(false);
 //        sub.setChecked(false);
  //       movebutton();
+        //zoom();
+        fade();
+        //moveViewToScreenCenter();
+
+    }
+    public void fade(){
+        Button image4 = (Button)findViewById(R.id.add);
+        Button image5 = (Button)findViewById(R.id.sub);
+        TextView image3 = (TextView)findViewById(R.id.textView3);
+        EditText image = (EditText)findViewById(R.id.Name3);
+        EditText image2 = (EditText)findViewById(R.id.Entry3);
+        TextView image6 = (TextView)findViewById(R.id.addmember);
+        Animation animation1 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade);
+        Animation animation2 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade2);
+        Animation animation3 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fadeout);
+        image.startAnimation(animation2);
+        image2.startAnimation(animation2);
+        image3.startAnimation(animation2);
+        image4.startAnimation(animation3);
+        image5.startAnimation(animation2);
+        image6.startAnimation(animation3);
     }
 
+    public void pop(){
+        TextView image3 = (TextView)findViewById(R.id.msg);
+        Animation animation1 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fadeinout);
+        image3.startAnimation(animation1);
+    }
+
+    public void fadeout(){
+        Button image5 = (Button)findViewById(R.id.sub);
+        TextView image3 = (TextView)findViewById(R.id.textView3);
+        EditText image = (EditText)findViewById(R.id.Name3);
+        EditText image2 = (EditText)findViewById(R.id.Entry3);
+        TextView image4 = (TextView)findViewById(R.id.addmember);
+        Button image6 = (Button)findViewById(R.id.add);
+        Animation animation1 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fadeout);
+        Animation animation2 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade2);
+        image.startAnimation(animation1);
+        image2.startAnimation(animation1);
+        image3.startAnimation(animation1);
+        image4.startAnimation(animation2);
+        image5.startAnimation(animation1);
+        image6.startAnimation(animation2);
+    }
+
+    public void zoom(){
+        Button image = (Button)findViewById(R.id.add);
+        Animation animation1 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.clockwise);
+        image.startAnimation(animation1);
+    }
+
+    private void moveViewToScreenCenter()
+    {   Button view = (Button)findViewById(R.id.add);
+        RelativeLayout root = (RelativeLayout) findViewById( R.id.mainlayout);
+        DisplayMetrics dm = new DisplayMetrics();
+        this.getWindowManager().getDefaultDisplay().getMetrics( dm );
+        int statusBarOffset = dm.heightPixels - root.getMeasuredHeight();
+
+        int originalPos[] = new int[2];
+        view.getLocationOnScreen( originalPos );
+
+        int xDest = dm.widthPixels/2;
+        xDest -= (view.getMeasuredWidth()/2);
+        int yDest = dm.heightPixels/2 - (view.getMeasuredHeight()/2) - statusBarOffset;
+
+        TranslateAnimation anim = new TranslateAnimation( 0, xDest - originalPos[0] , 0, yDest - originalPos[1] );
+        anim.setDuration(1000);
+        anim.setFillAfter( true );
+        view.startAnimation(anim);
+    }
 //    public void movebutton(){
 //    View PlusButton = findViewById(R.id.add);
 //     //Change the size of the button
@@ -146,6 +220,7 @@ public class MainActivity extends AppCompatActivity {
         entry.setVisibility(View.INVISIBLE);
         sub.setVisibility(View.INVISIBLE);
         add.setVisibility(View.VISIBLE);
+        fadeout();
     }
 
 
@@ -214,24 +289,29 @@ public class MainActivity extends AppCompatActivity {
             Name3.setText("");Entry3.setText("");
         }
         if(!checker.teamname(teamname.getText().toString()))
-        {   message.setVisibility(View.VISIBLE);
-            Log.i("this",teamname.getText().toString());
-            message.setText("TeamName should contain only Albhabets");
+        {   message.setVisibility(View.INVISIBLE);
+            Log.i("this", teamname.getText().toString());
+            message.setText("TeamName should contain only Alphabets");
+            pop();
         }
         else if(!((checker.NAME(Name1.getText().toString()))&&(checker.NAME(Name1.getText().toString()))&&(checker.NAME(Name1.getText().toString()))))
-        {   message.setVisibility(View.VISIBLE);
-            Log.i("this",Name1.getText().toString());
+        {   message.setVisibility(View.INVISIBLE);
+            Log.i("this", Name1.getText().toString());
             message.setText("Enter a valid name");
+            pop();
         }
         else if(!((checker.EntryNo(Entry1.getText().toString(),false))&&checker.EntryNo(Entry2.getText().toString(),false)&&checker.EntryNo(Entry3.getText().toString(),b)))
         { Log.i("this",Entry1.getText().toString());
-            message.setVisibility(View.VISIBLE);
-            message.setText("Enter a valid Entry Number");}
+            message.setVisibility(View.INVISIBLE);
+            message.setText("Enter a valid Entry Number");
+            pop();
+        }
 
         //        Request.Method.POST :
         else if(!checker.Diff_EntryNo(Entry1.getText().toString(),Entry2.getText().toString(),Entry3.getText().toString(),b))
-        {   message.setVisibility(View.VISIBLE);
+        {   message.setVisibility(View.INVISIBLE);
             message.setText("All Entry no. should be different");
+            pop();
         }
         else{
             StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
