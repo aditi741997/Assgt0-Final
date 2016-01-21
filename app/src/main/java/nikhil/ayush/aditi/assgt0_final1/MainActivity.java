f2f0dd1834447873cdbead07cc021c58c864db28
U-Ayush\Ayush <ayushb647@gmail.com>
2016-01-21 16:08:22 +0530

    Action bar menu added

--- app/src/main/java/nikhil/ayush/aditi/assgt0_final1/MainActivity.java
+++ app/src/main/java/nikhil/ayush/aditi/assgt0_final1/MainActivity.java
@@ -67,8 +67,8 @@ public class MainActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         //      Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
 
